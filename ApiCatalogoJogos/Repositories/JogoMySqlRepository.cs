634d9acf6d29a0e7be79e6b58706e804667ae59a
Victor Henrique Malta <victor.fobos@gmail.com>
2021-09-18 09:43:39 -0300

    Commit da API

---
+++ ApiCatalogoJogos/Repositories/JogoMySqlRepository.cs
@@ -0,0 +1,133 @@
+﻿using ApiCatalogoJogos.Entities;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.Repositories
+{
+    public class JogoMySqlRepository : IJogoRepository
+    {
+        private readonly MySqlConnection mySqlConnection;
+
+        public JogoMySqlRepository(IConfiguration configuration)
+        {
+            mySqlConnection = new MySqlConnection(configuration.GetConnectionString("Default"));
+        }
+        public async Task Atualizar(Jogo jogo)
+        {
+            var comando = $"UPDATE catalogo SET Nome = '{jogo.Nome}', Produtora = '{jogo.Produtora}', Preco = '{jogo.Preco.ToString().Replace(",", ".")}' WHERE Id = '{jogo.Id}'";
+
+            await mySqlConnection.OpenAsync();
+            MySqlCommand mySqlCommand = new MySqlCommand(comando, mySqlConnection);
+            mySqlCommand.ExecuteNonQuery();
+            await mySqlConnection.CloseAsync();
+        }
+
+        public void Dispose()
+        {
+            mySqlConnection?.Close();
+            mySqlConnection?.Dispose();
+        }
+
+        public async Task Inserir(Jogo jogo)
+        {
+            var comando = $"INSERT catalogo (Id, Nome, Produtora, Preco) VALUES ('{jogo.Id}', '{jogo.Nome}', '{jogo.Produtora}', '{jogo.Preco.ToString().Replace(",", ".")}')";
+            
+            await mySqlConnection.OpenAsync();
+            MySqlCommand mySqlCommand = new MySqlCommand(comando, mySqlConnection);
+            mySqlCommand.ExecuteNonQuery();
+            await mySqlConnection.CloseAsync();
+        }
+
+        public async Task<List<Jogo>> Obter(int pagina, int quantidade)
+        {
+            var jogos = new List<Jogo>();
+
+            var comandos = $"SELECT * FROM catalogo ORDER BY Id LIMIT {quantidade} OFFSET {(pagina - 1) * quantidade}";
+
+            await mySqlConnection.OpenAsync();
+            MySqlCommand mySqlCommand = new MySqlCommand(comandos, mySqlConnection);
+            MySqlDataReader mySqlDataReader = (MySqlDataReader)await mySqlCommand.ExecuteReaderAsync();
+
+            while (mySqlDataReader.Read())
+            {
+                jogos.Add(new Jogo
+                {
+                    Id = Guid.Parse((string)mySqlDataReader["Id"]),
+                    Nome = (string)mySqlDataReader["Nome"],
+                    Produtora = (string)mySqlDataReader["Produtora"],
+                    Preco = (double)mySqlDataReader["Preco"]
+                });
+            }
+
+            await mySqlConnection.CloseAsync();
+
+            return jogos;
+        }
+
+        public async Task<Jogo> Obter(Guid id)
+        {
+            Jogo jogo = null;
+
+            var comandos = $"SELECT * FROM catalogo WHERE Id = '{id}'";
+
+            await mySqlConnection.OpenAsync();
+            MySqlCommand mySqlCommand = new MySqlCommand(comandos, mySqlConnection);
+            MySqlDataReader mySqlDataReader = (MySqlDataReader)await mySqlCommand.ExecuteReaderAsync();
+
+            while (mySqlDataReader.Read())
+            {
+                jogo = new Jogo
+                {
+                    Id = Guid.Parse((string)mySqlDataReader["Id"]),
+                    Nome = (string)mySqlDataReader["Nome"],
+                    Produtora = (string)mySqlDataReader["Produtora"],
+                    Preco = (double)mySqlDataReader["Preco"]
+                };
+            }
+
+            await mySqlConnection.CloseAsync();
+
+            return jogo;
+        }
+
+        public async Task<List<Jogo>> Obter(string nome, string produtora)
+        {
+            var jogos = new List<Jogo>();
+
+            var comandos = $"SELECT * FROM catalogo WHERE Nome = '{nome}' AND Produtora = '{produtora}'";
+
+            await mySqlConnection.OpenAsync();
+            MySqlCommand mySqlCommand = new MySqlCommand(comandos, mySqlConnection);
+            MySqlDataReader mySqlDataReader = (MySqlDataReader)await mySqlCommand.ExecuteReaderAsync();
+
+            while (mySqlDataReader.Read())
+            {
+                jogos.Add(new Jogo
+                {
+                    Id = Guid.Parse((string)mySqlDataReader["Id"]),
+                    Nome = (string)mySqlDataReader["Nome"],
+                    Produtora = (string)mySqlDataReader["Produtora"],
+                    Preco = (double)mySqlDataReader["Preco"]
+                });
+            }
+
+            await mySqlConnection.CloseAsync();
+
+            return jogos;
+        }
+
+        public async Task Remover(Guid id)
+        {
+            var comando = $"DELETE FROM catalogo WHERE Id = '{id}' ";
+
+            await mySqlConnection.OpenAsync();
+            MySqlCommand mySqlCommand = new MySqlCommand(comando, mySqlConnection);
+            mySqlCommand.ExecuteNonQuery();
+            await mySqlConnection.CloseAsync();
+        }
+    }
+}
---
+++ ApiCatalogoJogos/Repositories/JogoRepository.cs
@@ -0,0 +1,79 @@
+﻿using ApiCatalogoJogos.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.Repositories
+{
+    public class JogoRepository: IJogoRepository
+    {
+        private static Dictionary<Guid, Jogo> jogos = new Dictionary<Guid, Jogo>()
+        {
+            {Guid.Parse("ca33ab28-098c-4e5f-8f3a-d7c951da7936"), new Jogo{Id = Guid.Parse("ca33ab28-098c-4e5f-8f3a-d7c951da7936"), Nome = "Fifa 21", Produtora = "EA", Preco = 200} },
+            {Guid.Parse("9b01f8eb-12e8-4bfa-979f-f62a1b8f0f86"), new Jogo{Id = Guid.Parse("9b01f8eb-12e8-4bfa-979f-f62a1b8f0f86"), Nome = "Fifa 20", Produtora = "EA", Preco = 190} },
+            {Guid.Parse("8bbfd389-d805-4ed8-a464-6b5f8515632f"), new Jogo{Id = Guid.Parse("8bbfd389-d805-4ed8-a464-6b5f8515632f"), Nome = "Fifa 19", Produtora = "EA", Preco = 180} },
+            {Guid.Parse("42fe971d-fe26-461f-8801-ee6255998341"), new Jogo{Id = Guid.Parse("42fe971d-fe26-461f-8801-ee6255998341"), Nome = "Fifa 18", Produtora = "EA", Preco = 170} },
+            {Guid.Parse("889ca674-22f2-4063-af39-d8b89e3aab36"), new Jogo{Id = Guid.Parse("889ca674-22f2-4063-af39-d8b89e3aab36"), Nome = "Street Fighter V", Produtora = "Capcom", Preco = 80} },
+            {Guid.Parse("50a37d07-5ac4-4388-a730-277040523680"), new Jogo{Id = Guid.Parse("50a37d07-5ac4-4388-a730-277040523680"), Nome = "Grand Theft Auto V", Produtora = "Rockstar", Preco = 190} }
+        };
+
+        public Task<List<Jogo>> Obter(int pagina, int quantidade)
+        {
+            return Task.FromResult(jogos.Values.Skip((pagina - 1) * quantidade).Take(quantidade).ToList());
+        }
+
+        public Task<Jogo> Obter(Guid id)
+        {
+            if (!jogos.ContainsKey(id))
+            {
+                return null;
+            }
+
+            return Task.FromResult(jogos[id]);
+        }
+
+        public Task<List<Jogo>> Obter(string nome, string produtora)
+        {
+            return Task.FromResult(jogos.Values.Where(jogo => jogo.Nome.Equals(nome) && jogo.Produtora.Equals(produtora)).ToList());
+        }
+
+        public Task<List<Jogo>> ObterSemLambda(string nome, string produtora)
+        {
+            var retorno = new List<Jogo>();
+
+            foreach (var jogo in jogos.Values)
+            {
+                if (jogo.Nome.Equals(nome) && jogo.Produtora.Equals(produtora))
+                {
+                    retorno.Add(jogo);
+                }
+            }
+
+            return Task.FromResult(retorno);
+        }
+
+        public Task Inserir(Jogo jogo)
+        {
+            jogos.Add(jogo.Id, jogo);
+            return Task.CompletedTask;
+        }
+
+        public Task Atualizar(Jogo jogo)
+        {
+            jogos[jogo.Id] = jogo;
+            return Task.CompletedTask;
+        }
+
+        public Task Remover(Guid id)
+        {
+            jogos.Remove(id);
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            //Fechar conexão com o banco se tivessemos.
+        }
+    }
+}
